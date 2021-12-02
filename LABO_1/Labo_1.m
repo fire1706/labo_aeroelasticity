fc30b1a095196b6fb49110ec5eb7279c201692e8
fire1706 <46523392+fire1706@users.noreply.github.com>
2021-12-02 20:09:30 +0100

    Update Labo_1.m

--- LABO_1/Labo_1.m
+++ LABO_1/Labo_1.m
@@ -54,8 +54,8 @@ for i = 1:7
     windowidth = (index(2,i)-index(1,i))*T_sampling;
     t = 0:T_sampling:windowidth;
     
-    smooth_plunge = sgolayfilt(plunge(i,index(1,i):index(2,i)),9,27);
-    smooth_pitch = sgolayfilt(pitch(i,index(1,i):index(2,i)),9,27);
+    smooth_plunge =plunge(i,index_bonus(1,i):index_bonus(2,i));%sgolayfilt(plunge(i,index_bonus(1,i):index_bonus(2,i)),9,27);
+    smooth_pitch =pitch(i,index_bonus(1,i):index_bonus(2,i));%sgolayfilt(pitch(i,index_bonus(1,i):index_bonus(2,i)),9,27);
     
     fft_plunge = fft(smooth_plunge);
     fft_pitch = fft(smooth_pitch);
@@ -72,8 +72,8 @@ for i = 1:7
      
     %freq_range = linspace(1*2*pi,201.03*2*pi,length(fft_plunge));
     
-    %[a,dplunge(:,:)] = rfp(P1_plunge,f*2*pi,2);
-    %[b,dpitch(:,:)] = rfp(P1_pitch,f*2*pi,2);
+    [a,dplunge(:,:)] = rfp(P1_plunge,f*2*pi,4);
+    [b,dpitch(:,:)] = rfp(P1_pitch,f*2*pi,4);
 %      [Max_plunge,ii] = max(P1_plunge);
 %      val_int_plunge = Max_plunge/sqrt(2)
 %      wn = f(ii)
@@ -98,11 +98,11 @@ for i = 1:7
     
     
     
-%     freq_plunge(i,:) = dplunge(1,:)/2/pi;% in Hz
-%     damp_plunge(i,:) = dplunge(2,:);% the two other quantity are seems to be useless
-% 
-%     freq_pitch(i,:) = dpitch(:,1)/2/pi;% in Hz
-%     damp_pitch(i,:) = dpitch(:,2);
+    freq_plunge(i,:) = dplunge(1,:)/2/pi% in Hz
+    damp_plunge(i,:) = dplunge(2,:)% the two other quantity are seems to be useless
+
+    freq_pitch(i,:) = dpitch(:,1)/2/pi% in Hz
+    damp_pitch(i,:) = dpitch(:,2)
 end
 Damp_HP_plunge = [0.05604,0.0871,0.1172,0.05961,0.0561,0.05104,0.00557];
 Damp_HP_pitch = [0.06132,0.0779,0.08229,0.0933,0.0598,0.0629,0.0106];
@@ -114,8 +114,10 @@ figure('name','frequency')
 hold on
 xlabel('Velocity')
 ylabel('Frequency')
-plot(airspeed,Freq_HP_plunge(:))
-plot(airspeed,Freq_HP_pitch(:))
+% plot(airspeed,Freq_HP_plunge(:))
+% plot(airspeed,Freq_HP_pitch(:))
+plot(airspeed,freq_plunge(:,1))
+plot(airspeed,freq_pitch(:,1))
 grid on
 legend('plunge','pitch')
 
@@ -124,8 +126,65 @@ figure('name','damping')
 hold on
 xlabel('Velocity')
 ylabel('Damping')
-plot(airspeed,Damp_HP_plunge(:))
-plot(airspeed,Damp_HP_pitch(:))
+% plot(airspeed,Damp_HP_plunge(:))
+% plot(airspeed,Damp_HP_pitch(:))
+plot(airspeed,damp_plunge(:,1))
+plot(airspeed,damp_pitch(:,1))
 grid on
 legend('plunge','pitch')
 
+
+%% ---------------------------
+%Partie Raph
+%%-------------
+
+%% Stability criterion part
+
+% Stability criterion of lab data -----------------------------------------
+S_pitch = zeros(7,1);
+S_plunge = zeros(7,1);
+for i = 1:7
+    y_pitch = pitch(i,index_bonus(1,i):index_bonus(2,i)); % [rad/s?] Pitch signal
+    y_plunge = plunge(i,index_bonus(1,i):index_bonus(2,i)); % [m/s?] Plunge signal
+
+    Y_pitch = fft(y_pitch); % Y(omega) FFT for pitch
+    Y_plunge = fft(y_plunge); % Y(omega) FFT for plunge
+    yh_pitch = ifft(imag(Y_pitch) - 1i*real(Y_pitch)); % yh(t) Hilbert Transform for pitch
+    yh_plunge = ifft(imag(Y_plunge) - 1i*real(Y_plunge)); % yh(t) Hilbert Transform for plunge
+
+    E_pitch = sqrt(y_pitch.^2 - yh_pitch.^2); % E(t) Envelope function for pitch
+    E_plunge = sqrt(y_plunge.^2 - yh_plunge.^2); % E(t) Envelope function for plunge
+    
+    t = (1:length(E_pitch))*T_sampling; % [s] Time steps
+    t1 = max(t); % [s] End time 
+
+    t_bar_pitch = sum(E_pitch.*t*T_sampling)/sum(E_pitch*T_sampling); % Time centroid for pitch
+    t_bar_plunge = sum(E_plunge.*t*T_sampling)/sum(E_plunge*T_sampling); % Time centroid for plunge
+
+    S_pitch(i) = real(1/t_bar_pitch - 2/t1);
+    S_plunge(i) = real(1/t_bar_plunge - 2/t1);
+end
+
+% 2nd order interpolations ------------------------------------------------
+coeffs_pitch = polyfit(airspeed', S_pitch, 2);
+coeffs_plunge = polyfit(airspeed', S_plunge, 2);
+xPos = (0:0.1:21);
+interpolVal_pitch = polyval(coeffs_pitch, (0:0.1:21));
+interpolVal_plunge = polyval(coeffs_plunge, (0:0.1:21));
+
+% Plots -------------------------------------------------------------------
+figure;
+plot(airspeed,S_pitch,'bo');
+hold on;
+plot(airspeed,S_plunge,'ro');
+
+plot((0:0.1:21),interpolVal_pitch,'b');
+plot((0:0.1:21),interpolVal_plunge,'r');
+zeroLine = yline(0,':');
+
+xlabel('Airspeed $U$ [m/s]','Interpreter','latex','Fontsize',24);
+ylabel('Envelope function stability criterion $S$','Interpreter','latex','Fontsize',24);
+legend('Pitch data', 'Plunge data', 'Pitch $2^{nd}$ order fit','Plunge $2^{nd}$ order fit','Interpreter','latex')
+set(gca,'fontsize',24,'TickLabelInterpreter','latex')
+set(zeroLine,'linewidth',1.5);
+grid on;
